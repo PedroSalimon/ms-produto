51fcc50934b1322c987cc5bca74e617266c1dc41
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-25 21:59:32 -0300

    Finalizando aula 3

--- src/main/java/com/github/cidarosa/ms/produto/controller/ProdutoController.java
+++ src/main/java/com/github/cidarosa/ms/produto/controller/ProdutoController.java
@@ -1,13 +1,15 @@
 package com.github.cidarosa.ms.produto.controller;
 
 import com.github.cidarosa.ms.produto.dto.ProdutoDTO;
+import com.github.cidarosa.ms.produto.entities.Produto;
 import com.github.cidarosa.ms.produto.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +23,35 @@ public class ProdutoController {
     public ResponseEntity<List<ProdutoDTO>> getAllProdutos(){
         List <ProdutoDTO> list = produtoService.findAllProdutos();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> getProdutoById(@PathVariable Long id){
+        ProdutoDTO produtoDTO = produtoService.findProdutoById(id);
+        return ResponseEntity.ok(produtoDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> createProduto (@RequestBody ProdutoDTO produtoDTO){
+        produtoDTO = produtoService.saveProduto(produtoDTO);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(produtoDTO.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(produtoDTO);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> updateProduto (@PathVariable Long id,
+                                                     @RequestBody ProdutoDTO produtoDTO) {
+        produtoDTO = produtoService.updateProduto(id, produtoDTO);
+        return ResponseEntity.ok(produtoDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduto (@PathVariable Long id) {
+        produtoService.deleteProdutoById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/github/cidarosa/ms/produto/service/ProdutoService.java
+++ src/main/java/com/github/cidarosa/ms/produto/service/ProdutoService.java
@@ -3,7 +3,8 @@ package com.github.cidarosa.ms.produto.service;
 import com.github.cidarosa.ms.produto.dto.ProdutoDTO;
 import com.github.cidarosa.ms.produto.entities.Produto;
 import com.github.cidarosa.ms.produto.repositories.ProdutoRepository;
-import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +16,52 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List <ProdutoDTO> findAllProdutos () {
         List < Produto> produtos = produtoRepository.findAll();
         return produtos.stream().map(ProdutoDTO::new).toList();
     }
 
+    @Transactional(readOnly = true)
+    public ProdutoDTO findProdutoById (Long id){
+        Produto produto = produtoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Recurso não encontrado. ID: " + id)
+        );
+
+        return new ProdutoDTO(produto);
+    }
+
+    @Transactional
+    public ProdutoDTO saveProduto (ProdutoDTO produtoDTO) {
+        Produto produto = new Produto();
+        copyDtoToProduto (produtoDTO, produto);
+        produto = produtoRepository.save(produto);
+        return new ProdutoDTO(produto);
+    }
+    private void copyDtoToProduto (ProdutoDTO produtoDTO, Produto produto){
+        produto.setNome(produtoDTO.getNome());
+        produto.setDescricao(produtoDTO.getDescricao());
+        produto.setValor(produtoDTO.getValor());
+    }
+
+     @Transactional
+     public ProdutoDTO updateProduto (Long id, ProdutoDTO produtoDTO) {
+        try {
+            Produto produto = produtoRepository.getReferenceById(id);
+            copyDtoToProduto(produtoDTO, produto);
+            produto = produtoRepository.save(produto);
+            return new ProdutoDTO(produto);
+        } catch (EntityNotFoundException e ){
+            throw new EntityNotFoundException("Recurso não encontrado. ID: " + id);
+        }
+     }
+
+     @Transactional
+    public void deleteProdutoById (Long id) {
+        if (!produtoRepository.existsById(id)){
+            throw new EntityNotFoundException("Recurso não encontrado. ID: " + id);
+        }
+        produtoRepository.deleteById(id);
+     }
 
 }
