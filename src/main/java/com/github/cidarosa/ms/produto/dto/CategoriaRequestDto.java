949c66ae80b1a7c84ea9514f0af00b1ead4b5889
Pedro Salimon Nascimento <127759985+PedroSalimon@users.noreply.github.com>
2026-08-05 20:51:15 -0300

    Atualizando o projeto seguindo a aula 13

--- src/main/java/com/github/cidarosa/ms/produto/dto/CategoriaRequestDto.java
+++ src/main/java/com/github/cidarosa/ms/produto/dto/CategoriaRequestDto.java
@@ -1,6 +1,7 @@
 package com.github.cidarosa.ms.produto.dto;
 
 import com.github.cidarosa.ms.produto.entities.Categoria;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,15 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class CategoriaDto {
+public class CategoriaRequestDto {
 
     private Long id;
 
     @NotBlank(message = "Campo nome não pode ser vazio, nulo ou em branco")
     @Size(min = 3, max = 100, message = "O campo nome deve ter entre 3 e 100 caracteres")
+    @Schema(example = "Ferramentas")
     private String nome;
 
-    public CategoriaDto(Categoria categoria) {
+    public CategoriaRequestDto(Categoria categoria) {
         id = categoria.getId();
         nome = categoria.getNome();
     }
---
+++ src/main/java/com/github/cidarosa/ms/produto/dto/ProdutoResponseDTO.java
@@ -0,0 +1,30 @@
+package com.github.cidarosa.ms.produto.dto;
+
+import com.github.cidarosa.ms.produto.entities.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProdutoResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double valor;
+    private CategoriaResponseDto categoria;
+
+    public ProdutoResponseDTO(Produto produto) {
+        id = produto.getId();
+        nome = produto.getNome();
+        descricao = produto.getDescricao();
+        valor = produto.getValor();
+        categoria = new CategoriaResponseDto(produto.getCategoria());
+    }
+}
